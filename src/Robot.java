b2482000725243167aa237ce1a64286b19b8dee3
MatthewSheppard1 <34612037+MatthewSheppard1@users.noreply.github.com>
2018-10-04 09:01:35 -0700

    added goForward and stop methods to Robot class

--- src/Robot.java
+++ src/Robot.java
@@ -9,9 +9,17 @@ public class Robot extends HummingbirdRobot {
     // Instance Variables go here
 
 
-    // Methods go here
+    // this method will set motor velocities to go in a forward moving direction
+    public void goForward(){
+        hBird.setMotorVelocity(1,255);
+        hBird.setMotorVelocity(2,-255);
+    }
 
-
+    // this method will set motor velocities to 0.
+    public void stop(){
+        hBird.setMotorVelocity(1,0);
+        hBird.setMotorVelocity(2,0);
+    }
     // this method will disconnect from the hummingbird and quit the program
     public void disconnect() {
         this.hBird.disconnect();
