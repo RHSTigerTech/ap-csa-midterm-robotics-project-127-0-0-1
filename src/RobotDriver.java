6e5e3aa49de326da20485beeb2e474b7c8c442b6
rhs-spencerfloyd <42384912+rhs-spencerfloyd@users.noreply.github.com>
2018-10-06 20:02:13 -0700

    more functionality to RobotDriver class
    added scanner instance
    ask for, print out user's name

--- src/RobotDriver.java
+++ src/RobotDriver.java
@@ -1,9 +1,22 @@
 
+import java.util.Scanner;
+
 public class RobotDriver {
     public static void main(String[] args) throws InterruptedException {
 
+        // create instance of Scanner
+        Scanner giveMeThe = new Scanner(System.in);
+
         // create instance of Robot
         Robot robot = new Robot();
+
+        // ask the user for their name
+        System.out.print("Hey what's your name? ");
+        String name = giveMeThe.nextLine();
+
+        // say hi in the chat
+        System.out.println();
+        System.out.println("Hello, " + name + "!");
 
         // test motors
         robot.goForward();
