e7e870879d0afa28f146e09f9771375ec46caa48
rhs-spencerfloyd <42384912+rhs-spencerfloyd@users.noreply.github.com>
2018-10-06 20:00:36 -0700

    add future methods, update go method
    added blinkEyes and rotate methods to implement in class
    changed goForward to go and made it work with a percent (-100 to 100) instead of -255 to 255

--- src/Robot.java
+++ src/Robot.java
@@ -13,15 +13,31 @@ public class Robot {
 
 
     // this method will set motor velocities to go in a forward moving direction
-    public void goForward(){
-        hBird.setMotorVelocity(1,255);
-        hBird.setMotorVelocity(2,-255);
+    // takes a percent -100-100 as input
+    public void go(int percent) {
+        if (percent >= -100 && percent <= 100) {
+            hBird.setMotorVelocity(1,(255.0/100.0) * percent);
+            hBird.setMotorVelocity(2,(255.0/100.0) * percent);
+        } else {
+            System.err.println("Can only accept values between -100 and 100.");
+        }
+    }
+    
+    // rotate around by specified degrees
+    public void rotate(int degrees) {
+        // implement this during class with trial and error
+        // best way to program duh
     }
 
     // this method will set motor velocities to 0.
-    public void stop(){
+    public void stop() {
         hBird.setMotorVelocity(1,0);
         hBird.setMotorVelocity(2,0);
+    }
+
+    // this method will make the eye leds turn on and off
+    public void blinkEyes() {
+        // to be implemented
     }
 
     // this method will disconnect from the hummingbird and quit the program
