ea57eddbe6eb66b33319b467948e54e82eb2caf4
rhs-spencerfloyd <spencer.floyd@student.rjuhsd.us>
2018-10-09 09:31:04 -0700

    change Robot inheritance to HummingbirdRobot

--- src/Robot.java
+++ src/Robot.java
@@ -4,14 +4,11 @@ import org.json.JSONException;
 
 import java.io.IOException;
 
-public class Robot {
-
-    // Instance Variables
-    private HummingbirdRobot hBird;
+public class Robot extends HummingbirdRobot {
 
     // Constructor to create instance of hummingbirdrobot
     public Robot() {
-        hBird = new HummingbirdRobot();
+        super();
     }
 
 
@@ -19,8 +16,8 @@ public class Robot {
     // takes a percent -100-100 as input
     public void go(int percent) {
         if (percent >= -100 && percent <= 100) {
-            hBird.setMotorVelocity(1,(int)(255.0/100.0) * percent);
-            hBird.setMotorVelocity(2,(int)(255.0/100.0) * percent);
+            this.setMotorVelocity(1,(int)(255.0/100.0) * percent);
+            this.setMotorVelocity(2,(int)(255.0/100.0) * percent);
         } else {
             System.err.println("Can only accept values between -100 and 100.");
         }
@@ -30,8 +27,8 @@ public class Robot {
     //takes a degree from 0 to 360 as input
     public void rotate(int degrees) {
         if(degrees >= 0 && degrees <= 360){
-            hBird.setMotorVelocity(2,(int)((degrees*(255.0/360.0))));
-            hBird.setMotorVelocity(2,-(int)((degrees*(255.0/360.0))));
+            this.setMotorVelocity(2,(int)((degrees*(255.0/360.0))));
+            this.setMotorVelocity(2,-(int)((degrees*(255.0/360.0))));
         } else {
             System.err.println("Can only accept values between 0 and 360.");
         }
@@ -39,8 +36,8 @@ public class Robot {
 
     // this method will set motor velocities to 0.
     public void stop() {
-        hBird.setMotorVelocity(1,0);
-        hBird.setMotorVelocity(2,0);
+        this.setMotorVelocity(1,0);
+        this.setMotorVelocity(2,0);
     }
 
     // this method will stop the robot if it detects something from a specified distance
@@ -53,7 +50,7 @@ public class Robot {
 
         // tolerance range for distance values
         for (int i = lowerLimit; i <= distance + tolerance; i++) {
-            if (hBird.getSensorValue(3) == distance) {
+            if (this.getSensorValue(3) == distance) {
                 // action after detecting something in the distance
 
                 this.blinkEyes();
@@ -64,26 +61,26 @@ public class Robot {
     // this method will make the eye leds turn on and off
     public void blinkEyes() throws InterruptedException{
         for(int i = 1; i < 5; i++) {
-            hBird.setLED(i,0);
+            this.setLED(i,0);
         }
         for(int i = 1; i < 3; i++) {
-            hBird.setFullColorLED(i,0,0,0);
+            this.setFullColorLED(i,0,0,0);
         }
         Thread.sleep(500);
         for(int i = 1; i < 5; i++) {
-            hBird.setLED(i,255);
+            this.setLED(i,255);
         }
         for(int i = 1; i < 3; i++) {
-            hBird.setFullColorLED(i,255,0,0);
+            this.setFullColorLED(i,255,0,0);
         }
     }
     //turns on all 6 eyes
     public void turnOnEyes(){
         for(int i = 1; i < 5; i++) {
-            hBird.setLED(i,255);
+            this.setLED(i,255);
         }
         for(int i = 1; i < 3; i++) {
-            hBird.setFullColorLED(i,255,0,0);
+            this.setFullColorLED(i,255,0,0);
         }
     }
 
@@ -91,18 +88,18 @@ public class Robot {
     //this method detects light value and increases or decreases brightness
     //of leds based on the sensor value
     public void detectLight(){
-        int s = 255 - hBird.getSensorValue(1);
+        int s = 255 - this.getSensorValue(1);
         for(int i = 1; i < 5; i++) {
-            hBird.setLED(i,s);
+            this.setLED(i,s);
         }
         for(int i = 1; i < 3; i++) {
-            hBird.setFullColorLED(i,s,0,0);
+            this.setFullColorLED(i,s,0,0);
         }
     }
 
     // this method takes a string as input and says it
     public void say(String myStr) {
-        hBird.speak(myStr);
+        this.speak(myStr);
     }
     public String translate(String endLang, String str) throws IOException, JSONException {
         return Translator.translate("en",endLang,str);
@@ -113,7 +110,7 @@ public class Robot {
         while(spotFound == false){
             go(100);
             for(int i = 0; i < 5000; i++) {
-                if(hBird.getSensorValue(2) >= 200 && hBird.getSensorValue(1) <= 200){
+                if(this.getSensorValue(2) >= 200 && this.getSensorValue(1) <= 200){
                     stop();
                     rotate(150);
                     spotFound = true;
@@ -125,8 +122,8 @@ public class Robot {
     }
 
     // this method will disconnect from the hummingbird and quit the program
-    public void disconnect() {
-        this.hBird.disconnect();
-    }
+    //public void disconnect() {
+    //    this.hBird.disconnect();
+    //}
 
 }
