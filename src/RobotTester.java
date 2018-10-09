44937440aded2fa6a92d785f4fcbe359cdee1d9e
rhs-spencerfloyd <spencer.floyd@student.rjuhsd.us>
2018-10-09 08:19:56 -0700

    move testing over to RobotTester class

---
+++ src/RobotTester.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class RobotTester {
+    public static void main(String[] args) throws InterruptedException {
+
+        // create instance of Scanner
+        Scanner giveMeThe = new Scanner(System.in);
+
+        // create instance of Robot
+        Robot robot = new Robot();
+
+        // ask the user for their name
+        //System.out.print("Hey what's your name? ");
+        // String name = giveMeThe.nextLine();
+
+        // say hi in the chat
+        System.out.println();
+        //System.out.println("Hello, " + name + "!");
+        robot.turnOnEyes();
+        Thread.sleep(10000);
+        robot.blinkEyes();
+        Thread.sleep(10000);
+        robot.disconnect();
+        // test motors
+        /*
+        robot.go(100);
+        Thread.sleep(25000);
+        robot.rotate(90);
+        robot.go(100);
+        Thread.sleep(25000);
+        robot.stop();
+        */
+
+    }
+}
