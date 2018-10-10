b17a117439b0b5be5e0ea4e60d3305ff312f4710
MatthewSheppard1 <34612037+MatthewSheppard1@users.noreply.github.com>
2018-10-10 14:51:34 -0700

    turned on eyes in driver program

--- src/RobotDriver.java
+++ src/RobotDriver.java
@@ -25,18 +25,18 @@ public class RobotDriver {
 
         // say hi in the chat
         System.out.println();
-        Translator.tprintln(lang,"Hello, " + name + "!");
-        Translator.tprintln(lang,"I am speaking in the language that you selected!");
+        UI.tprintln(lang,"Hello, " + name + "!");
+        UI.tprintln(lang,"I am speaking in the language that you selected!");
         if (!lang.equals("en")) {
-            Translator.tprintln(lang,"It may take a split second for each line to appear, as the robot has to translate.");
+            UI.tprintln(lang,"It may take a split second for each line to appear, as the robot has to translate.");
         }
         System.out.println();
 
         // ask for a mission
-        Translator.tprintln(lang,"What do you want me to do?");
-        Translator.tprintln(lang,"1. Sing a song in your language");
-        Translator.tprintln(lang,"2. Find a good hiding spot");
-        Translator.tprintln(lang,"3. You give me something to say");
+        UI.tprintln(lang,"What do you want me to do?");
+        UI.tprintln(lang,"1. Sing a song in your language");
+        UI.tprintln(lang,"2. Find a good hiding spot");
+        UI.tprintln(lang,"3. You give me something to say");
 
         // get the user's selection
         int selection = UI.tSelectionInt(lang, 3);
@@ -45,11 +45,19 @@ public class RobotDriver {
         System.out.println();
         if (selection == 1) {
             // sing song
+            UI.tprintln(lang, "Happy Birthday to You, cha, cha, cha.\n" +
+                    "Happy Birthday to You, cha, cha, cha.\n" +
+                    "Happy Birthday Dear " + name +
+                    "\nHappy Birthday to You, cha, cha, cha.");
+            robot.say(Translator.translate("en",lang,"Happy Birthday to You, cha, cha, cha.\n" +
+                    "Happy Birthday to You, cha, cha, cha.\n" +
+                    "Happy Birthday Dear " + name +
+                    "\nHappy Birthday to You, cha, cha, cha."));
         } else if (selection == 2) {
             // find hiding spot
         } else if (selection == 3) {
             // give something for robot to say
-            Translator.tprintln(lang,"Okay, what do you want me to say? ");
+            UI.tprintln(lang,"Okay, what do you want me to say? ");
             String sayThis = giveMeThe.nextLine();
             sayThis = giveMeThe.nextLine();
             if (!lang.equals("en"))
